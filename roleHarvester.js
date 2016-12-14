7c7c18028bbf17966d46f5761090ea20d0c79a4c
SotoDucani <jdvh1292@gmail.com>
2016-12-14 00:19:44 -0500

    Working, don't like it.

--- roleHarvester.js
+++ roleHarvester.js
@@ -3,12 +3,6 @@ var roleHarvester = {
   /** @param {Creep} creep **/
   run: function(creep) {
 
-    var targets = creep.room.find(FIND_STRUCTURES, {
-      filter: (structure) => {
-        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-      }
-    });
-
 	  if(creep.carry.energy < creep.carryCapacity) {
       creep.memory.working = true;
       var sources = creep.room.find(FIND_SOURCES);
@@ -17,22 +11,17 @@ var roleHarvester = {
       }
     }
 
-    else if (creep.carry.energy == creep.carryCapacity && targets.length > 0) {
-      creep.memory.working = true;
-      if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(targets[0]);
-      }
-    }
-
     else {
-      creep.memory.working = false;
-      targetSpawn = creep.room.find(FIND_STRUCTURES, {
-        filter: (strucure) => {
-          return (structure.structureType == STRUCTURE_SPAWN);
+      var targets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
         }
       });
-      if (targetSpawn.length > 0) {
-        creep.moveTo(targetSpawn[0]);
+      if(targets.length > 0) {
+        creep.memory.working = true;
+        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0]);
+        }
       }
     }
 	}
