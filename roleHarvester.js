7c0d76563a1b20ea5475c54747f184d4b885c587
SotoDucani <jdvh1292@gmail.com>
2016-12-14 00:45:18 -0500

    Working harvester
    Note: Hardcoded Spawn

--- roleHarvester.js
+++ roleHarvester.js
@@ -2,26 +2,25 @@ var roleHarvester = {
 
   /** @param {Creep} creep **/
   run: function(creep) {
-
-	  if(creep.carry.energy < creep.carryCapacity) {
+    //If creep is transfering and runs out of energy
+    if (creep.memory.working == true && creep.carry.energy == 0) {
+      creep.memory.working = false;
+    }
+    //If creep is gathering and fills it's capacity
+    else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity) {
       creep.memory.working = true;
-      var sources = creep.room.find(FIND_SOURCES);
-      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(sources[0]);
+    }
+    //If creep needs to transfer energy to spawn
+    if (creep.memory.working == true) {
+      if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(Game.spawns.Spawn1);
       }
     }
-
-    else {
-      var targets = creep.room.find(FIND_STRUCTURES, {
-        filter: (structure) => {
-          return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.energy < structure.energyCapacity;
-        }
-      });
-      if(targets.length > 0) {
-        creep.memory.working = true;
-        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0]);
-        }
+    //If creep needs to gather energy from a source
+    else if (creep.memory.working == false) {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES);
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(source);
       }
     }
 	}
