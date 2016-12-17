247e356ea4b81a3b8cfb30f12aa64ee1d0d85dfc
SotoDucani <jdvh1292@gmail.com>
2016-12-16 23:07:32 -0500

    Revert "Fix conflict"
    This reverts commit b7986e4f1ad0305b9633172df0d1559315711e7f.

--- roleHarvester.js
+++ roleHarvester.js
@@ -29,7 +29,7 @@ var roleHarvester = {
           }
         }
       }
-      if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      else if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(Game.spawns.Spawn1);
       }
     }
