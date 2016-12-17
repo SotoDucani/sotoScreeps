14045bc07a050cf90d7ec0602fca5f23b69683b7
SotoDucani <jdvh1292@gmail.com>
2016-12-16 23:08:16 -0500

    Revert "Remove duplicate code"
    This reverts commit 728ae4cacf5f83ae246d7b75cfe5f696777d075b.

--- roleHarvester.js
+++ roleHarvester.js
@@ -27,6 +27,11 @@ var roleHarvester = {
             }
           }
         }
+        if (containerTarget[0].energy < containerTarget[0].energyCapacity) {
+          if (creep.transfer(containerTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(containerTarget);
+          }
+        }
       }
       else if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(Game.spawns.Spawn1);
