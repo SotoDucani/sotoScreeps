b7986e4f1ad0305b9633172df0d1559315711e7f
SotoDucani <jdvh1292@gmail.com>
2016-12-16 23:04:56 -0500

    Fix conflict

--- roleHarvester.js
+++ roleHarvester.js
@@ -29,7 +29,7 @@ var roleHarvester = {
           }
         }
       }
-      else if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+      if (creep.transfer(Game.spawns.Spawn1, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
         creep.moveTo(Game.spawns.Spawn1);
       }
     }
