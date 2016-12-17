97c7891e63799da88e6d0994fced6cdd84280897
SotoDucani <jdvh1292@gmail.com>
2016-12-17 14:51:06 -0500

    New role Repairer; Fix target is not defined

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 var roleHarvester = require('roleHarvester');
 var roleUpgrader = require('roleUpgrader');
 var roleBuilder = require('roleBuilder');
+var roleRepairer = require('roleRepairer');
 //var roleMover = require('roleMover');
 
 module.exports.loop = function () {
   var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
   var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
   var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+  var repairers = _.filter(Game.creeps, (creep) => creep.memory.role == 'repairer');
   //var movers = _.filter(Game.creeps, (creep) => creep.memory.role == 'mover');
 
   //Spawn creeps in order
   if(harvesters.length < 8) {
-    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester', working: false, target});
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester', working: false, target: ""});
   }
   else if(upgraders.length < 4) {
-    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', upgrading: false, target});
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', upgrading: false, target: ""});
   }
   else if(builders.length < 6) {
-    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder', building: false, target});
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'builder', building: false, target: ""});
+  }
+  else if(repairers.length < 6) {
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'repairer', repairing: false, target: ""});
   }
   //else if(movers.length < 2) {
   //  var newName = Game.spawns['Spawn1'].createCreep([CARRY,CARRY,MOVE,MOVE]), undefined, {role: 'mover', moving: false, target});
@@ -37,6 +42,10 @@ module.exports.loop = function () {
     else if(creep.memory.role == 'builder') {
       //creep.say("I'm a builder!");
       roleBuilder.run(creep);
+    }
+    else if(creep.memory.role == 'repairer') {
+      //creep.say("I'm a repairer!");
+      roleRepairer.run(creep);
     }
     //else if(creep.memory.role == 'mover') {
       //creep.say("I'm a mover!");
---
+++ roleRepairer.js
@@ -0,0 +1,43 @@
+var roleRepairer = {
+
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    //If creep is repairing and runs out of energy
+  	if(creep.memory.repairing == true && creep.carry.energy == 0) {
+      creep.memory.repairing = false;
+      //creep.say("Swap to harvesting!");
+  	}
+    //If creep is gathering and fills it's capacity
+  	else if(creep.memory.repairing == false && creep.carry.energy == creep.carryCapacity) {
+  	  creep.memory.repairing = true;
+  	  //creep.say("Swap to repairing!");
+  	}
+    //If creep needs to use energy to repair structures
+  	if(creep.memory.repairing == true) {
+  	  var targets = creep.room.find(FIND_STRUCTURES);
+      if(targets.length > 0) {
+        //creep.say("Repairing!");
+        creep.memory.target = targets[0];
+        if(creep.repair(targets[0]) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets[0]);
+        }
+      }
+    }
+    //If creep needs to gather energy
+    else if (creep.memory.repairing == false) {
+      var withdrawTargets = creep.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          return (structure.structureType == STRUCTURE_CONTAINER ||
+            structure.structureType == STRUCTURE_STORAGE) && structure.store[RESOURCE_ENERGY] > 100;
+        }
+      });
+      //creep.say("Withdrawing energy!");
+      creep.memory.target = withdrawTargets[0];
+      if (creep.withdraw(withdrawTargets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(withdrawTargets[0]);
+      }
+    }
+  }
+}
+
+module.exports = roleRepairer;
