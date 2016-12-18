e6b869f5464572191c63bbdbd57048c450c67225
SotoDucani <jdvh1292@gmail.com>
2016-12-17 20:22:44 -0500

    Switch mover to findClosestByRange

--- roleMover.js
+++ roleMover.js
@@ -14,16 +14,16 @@ var roleMover = {
 
     //If Creep has energy to move
     if (creep.memory.moving == true) {
-      var targets = creep.room.find(FIND_STRUCTURES, {
+      var target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
         filter: (structure) => {
           return (structure.structureType == STRUCTURE_EXTENSION ||
             structure.structureType == STRUCTURE_SPAWN) &&
             structure.energy < structure.energyCapacity;
         }
       });
-      if(targets.length > 0) {
-        if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets[0]);
+      if(target.length) {
+        if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(targets);
         }
       }
       //Can't find targets, but have energy
@@ -35,14 +35,14 @@ var roleMover = {
 
     //If the creep has no energy to move, get some from a container if there is a need
     else if (creep.memory.moving == false) {
-      var withdrawTarget = creep.room.find(FIND_STRUCTURES, {
+      var withdrawTarget = creep.pos.findClosestByRange(FIND_STRUCTURES, {
         filter: function(structure) {
           return structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] >= creep.carryCapacity;
         }
       });
-      if (withdrawTarget.length > 0) {
-        if (creep.withdraw(withdrawTarget[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(withdrawTarget[0]);
+      if (withdrawTarget) {
+        if (creep.withdraw(withdrawTarget, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(withdrawTarget);
         }
       }
       else {
