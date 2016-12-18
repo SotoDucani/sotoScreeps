97f1569f709a9cfe4a642d583257288de3462f87
SotoDucani <jdvh1292@gmail.com>
2016-12-17 19:33:05 -0500

    Make big Harvester

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ module.exports.loop = function () {
 
   //Spawn creeps in order
   if(harvesters.length < 5) {
-    var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,WORK,MOVE], undefined, {role: 'harvester', working: false, target: ""});
+    var newName = Game.spawns['Spawn1'].createCreep([WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE], undefined, {role: 'harvester', working: false, target: ""});
   }
   else if(upgraders.length < 2) {
     var newName = Game.spawns['Spawn1'].createCreep([WORK,CARRY,MOVE,MOVE], undefined, {role: 'upgrader', upgrading: false, target: ""});
